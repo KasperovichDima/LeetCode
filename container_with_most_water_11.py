243ac174feb02e4d596ca6793adc165c9c0ff136
kasper <dimakasperovich1720@gmail.com>
2026-02-24 06:10:21 -0800

    container task updated

--- container_with_most_water_11.py
+++ container_with_most_water_11.py
@@ -1,23 +1,20 @@
 class Solution:
     def maxArea(self, height: list[int]) -> int:
-        left_ptr = 0
-        right_ptr = bottom_len =len(height) - 1
+        left = 0
+        right = len(height) - 1
 
-        min_side = min(height[left_ptr], height[right_ptr])
-        best = min_side * bottom_len
-
-        while left_ptr < right_ptr:
-            if min_side == height[left_ptr]:
-                left_ptr += 1
+        best = 0
+        while left < right:
+            current = min(height[right], height[left]) * (right - left)
+            best = max(best, current)
+            if height[left] < height[right]:
+                left += 1
             else:
-                right_ptr -= 1
-            bottom_len = right_ptr - left_ptr
-            min_side = min(height[left_ptr], height[right_ptr])
-            new_res = min_side * bottom_len
-            best = max(best, new_res)
+                right -= 1
 
         return best
 
+
 s = Solution()
-assert s.max_area([1,8,6,2,5,4,8,3,7]) == 49
-assert s.max_area([1,1]) == 1
+assert s.maxArea([1,8,6,2,5,4,8,3,7]) == 49
+assert s.maxArea([1,1]) == 1
