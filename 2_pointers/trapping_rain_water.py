113008f0581adbcbd8290c29fc25a4e48ca02210
kasper <dimakasperovich1720@gmail.com>
2026-03-05 05:38:58 -0800

    trap solved

--- 2_pointers/trapping_rain_water.py
+++ 2_pointers/trapping_rain_water.py
@@ -1,37 +1,49 @@
-# class Solution:
-#     def trap(self, height: list[int]) -> int:
-#         left = result = 0
-        
-#         while left < len(height) - 1:
-#             if not left:
-#                 left += 1
-#                 continue
-#             right = left + 1
-#             if height[right] >= height[left]:
-#                 left += 1
-#                 continue
-
-#             # right wall found
-#             ...  # here is the main code to impl
-#             left = right
-
-
-
-#         return result
-
-
 class Solution:
     def trap(self, height: list[int]) -> int:
-        left_ind = right_ind = result = occupied = 0
-        gap_found = False
+        left = res = 0
+        right = len(height) - 1
+        top_left = height[left]
+        top_right = height[right]
+        while left < right:
+            if top_right < top_left:
+                if height[right] < top_right:
+                    res += top_right - height[right]
+                else:
+                    top_right = height[right]
+                right -= 1
+            else:
+                if height[left] < top_left:
+                    res += top_left - height[left]
+                else:
+                    top_left = height[left]
+                left += 1
 
-        for i in range(len(height)):
-            if height[i] > height[left_ind]:
-                # main logic
-                ... # upd res and switch left
-            elif not height[i]:
-                gap_found = True
-            elif height: # not 0 but lower than left
-                ... # upd right and occupied
+        return res
 
-          
+s = Solution()
+
+# The Standard Mix
+assert s.trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]) == 6
+
+# The "Descending Slope" (Your Nemesis)
+assert s.trap([10, 0, 9, 0, 8]) == 17
+
+# The "Staircase" (Should be Zero)
+assert s.trap([1, 2, 3, 4, 5]) == 0
+assert s.trap([5, 4, 3, 2, 1]) == 0
+
+# The "Twin Peaks" (Deep Valley)
+assert s.trap([5, 0, 0, 0, 5]) == 15
+
+# The "Middle Peak"
+assert s.trap([1, 2, 10, 2, 1]) == 0
+
+# The "Bowl"
+assert s.trap([3, 1, 2, 1, 4]) == 5
+
+# Empty/Single cases
+assert s.trap([5]) == 0
+assert s.trap([5, 2]) == 0
+
+# Large Gaps
+assert s.trap([4, 2, 0, 3, 2, 5]) == 9
