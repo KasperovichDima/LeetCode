cbb304520f1a84bb0744581b06c8cbd1886915e4
kasper <dimakasperovich1720@gmail.com>
2026-03-20 13:52:17 -0700

    maxOperations done

--- leetcode75/2_pointers/max_num_of_k_sum.py
+++ leetcode75/2_pointers/max_num_of_k_sum.py
@@ -1,3 +1,17 @@
 class Solution:
     def maxOperations(self, nums: list[int], k: int) -> int:
-        
+        nums.sort()
+        l = res = 0
+        r = len(nums) - 1
+        while l < r:
+            s = nums[l] + nums[r]
+            if s == k:
+                r -= 1
+                l += 1
+                res += 1
+            elif s > k:
+                r -= 1
+            else:
+                l += 1
+
+        return res
