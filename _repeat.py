2940610c0422ae023012a23c130a89c0845267a7
kasper <dimakasperovich1720@gmail.com>
2026-03-21 17:48:22 -0700

    solving some shit

--- _repeat.py
+++ _repeat.py
@@ -1,13 +1,31 @@
 class Solution:
-    def productExceptSelf(self, nums: list[int]) -> list[int]:
-        res = [1]
-        for i in range(len(nums) - 1):
-            res.append(nums[i] * res[i])
-        acc = 1
-        for i in range(len(nums) - 1, -1, -1):
-            res[i] *= acc
-            acc *= nums[i]
+    def longestOnes(self, nums: list[int], k: int) -> int:
+        res = zeros = left = right = 0
+        while right < len(nums):
+            if nums[right] == 0:
+                zeros += 1
+            if zeros > k:
+                left += 1
+                zeros -= nums[left]
+            else:
+                res = max(res, right - left)
+            right += 1
+
         return res
+
+
+
+        # zeros = i = 0
+        # while zeros < k and i < len(nums):
+        #     if nums[i] == 0:
+        #         zeros += 1
+        #     i += 1
+
+        # max_res = this_res = i
+       
+
+        # return max_res
     
+
 s = Solution()
-print(s.productExceptSelf([-1,1,0,-3,3]))
+assert s.longestOnes([1,1,1,0,0,0,1,1,1,1,0], k = 2) == 6
